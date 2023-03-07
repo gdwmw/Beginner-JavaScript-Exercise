c65dc04c93778dea3d1348f674d88dc62a2e1e4e
Landensaki <gededewowahyumw@gmail.com>
2023-03-08 00:09:01 +0700

    Update

--- src/document-object-model.js
+++ src/document-object-model.js
@@ -9,12 +9,6 @@ const para = document.querySelector("#para");
 para.setAttribute("class", "para");
 para.innerHTML =
   "Latihan Bahasa Pemrograman JavaScript, DOM, Asynchronous, AJAX, dan API.";
-// console.log(para.getAttribute("id"));
-
-const img = document.querySelector("#jsimg");
-img.setAttribute("class", "jsimg");
-img.src = "public/favicon.svg";
-// console.log(img.getAttribute("src"));
 
 const cLine = document.querySelectorAll("#color-line");
 cLine.forEach((line) => {
@@ -26,8 +20,23 @@ cLine.forEach((line) => {
   line.classList.add("add-class");
 });
 
+const img = document.querySelector("#jsimg");
+img.setAttribute("class", "jsimg");
+img.src = "public/favicon.svg";
+img.setAttribute("style", "display: block;");
+function fHU() {
+  if (img.style.display == "none") {
+    img.setAttribute("style", "display: block;");
+  } else {
+    img.setAttribute("style", "display: none;");
+  }
+}
+
+const hU = document.querySelector("#HU");
+hU.setAttribute("style", "display: block; margin: 30px auto 0 auto;");
+
 const gambar = document.querySelector("#gambar");
-gambar.setAttribute("style", "column-count: 5;");
+gambar.setAttribute("style", "column-count: 5; margin: 30px 0 0 0 ");
 for (let i = 1; i <= 15; i++) {
   const newGambar = document.createElement("img");
   newGambar.setAttribute("class", "margin");
@@ -36,5 +45,9 @@ for (let i = 1; i <= 15; i++) {
     "style",
     "width: 100px; height: auto; display: block; margin: 0 auto;"
   );
+  const span = document.createElement("span");
+  span.innerHTML = `#${i}`;
+  span.setAttribute("style", "display: block; text-align: center;");
   gambar.appendChild(newGambar);
+  gambar.appendChild(span);
 }
