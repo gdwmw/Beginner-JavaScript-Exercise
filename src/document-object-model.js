dd7d9c0369d8614aebd824a8236492757067d1a7
Landensaki <gededewowahyumw@gmail.com>
2023-03-07 23:09:13 +0700

    update

--- src/document-object-model.js
+++ src/document-object-model.js
@@ -16,10 +16,25 @@ img.setAttribute("class", "jsimg");
 img.src = "public/favicon.svg";
 // console.log(img.getAttribute("src"));
 
-const cLine = document.querySelector("#color-line");
-cLine.setAttribute(
-  "style",
-  "background-color: black; height: 2px; width: 100px; display: block; margin: 0 auto;"
-);
-cLine.setAttribute("class", "color-line");
-cLine.classList.add("add-class");
+const cLine = document.querySelectorAll("#color-line");
+cLine.forEach((line) => {
+  line.setAttribute(
+    "style",
+    "background-color: black; height: 2px; width: 100px; display: block; margin: 0 auto;"
+  );
+  line.setAttribute("class", "color-line");
+  line.classList.add("add-class");
+});
+
+const gambar = document.querySelector("#gambar");
+gambar.setAttribute("style", "column-count: 5;");
+for (let i = 1; i <= 15; i++) {
+  const newGambar = document.createElement("img");
+  newGambar.setAttribute("class", "margin");
+  newGambar.setAttribute("src", "public/favicon.svg");
+  newGambar.setAttribute(
+    "style",
+    "width: 100px; height: auto; display: block; margin: 0 auto;"
+  );
+  gambar.appendChild(newGambar);
+}
