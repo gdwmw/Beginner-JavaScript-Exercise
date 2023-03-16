5334f93de796d178d2dcf408bdd2ef0a1b290d9e
Landensaki <gededewowahyumw@gmail.com>
2023-03-17 00:12:23 +0700

    Update

--- src/js/score-board.js
+++ src/js/score-board.js
@@ -30,6 +30,20 @@ const h1P1 = document.createElement("h1");
 h1P1.setAttribute("class", "text-8xl text-white");
 h1P1.innerHTML = scoreP1;
 
+const winnersP1 = document.createElement("p");
+winnersP1.setAttribute("class", "mt-1 h-10 font-semibold text-2xl text-center");
+function fWinP1() {
+  if (gameOver === true) {
+    if (scoreP1 === target) {
+      winnersP1.classList.add("text-green-500");
+      winnersP1.innerHTML = "WINNER";
+    } else {
+      winnersP1.classList.add("text-red-500");
+      winnersP1.innerHTML = "LOSE";
+    }
+  }
+}
+
 //----------------------------
 
 const vs = document.createElement("h1");
@@ -54,6 +68,20 @@ const h1P2 = document.createElement("h1");
 h1P2.setAttribute("class", "text-8xl text-white");
 h1P2.innerHTML = scoreP2;
 
+const winnersP2 = document.createElement("p");
+winnersP2.setAttribute("class", "mt-1 h-10 font-semibold text-2xl text-center");
+function fWinP2() {
+  if (gameOver === true) {
+    if (scoreP2 === target) {
+      winnersP2.classList.add("text-green-500");
+      winnersP2.innerHTML = "WINNER";
+    } else {
+      winnersP2.classList.add("text-red-500");
+      winnersP2.innerHTML = "LOSE";
+    }
+  }
+}
+
 //----------------------------
 
 const button = document.getElementById("button");
@@ -65,14 +93,29 @@ btnP1.setAttribute(
   "mr-2 px-3 py-2 border-2 border-amber-400 hover:border-white hover:text-white rounded-lg bg-amber-400 font-semibold"
 );
 btnP1.innerHTML = "PLAYER 1<br>(+1)";
+window.addEventListener("keydown", (e) => {
+  if (e.key === ",") {
+    if (!gameOver) {
+      scoreP1 += 1;
+      if (scoreP1 === target) {
+        gameOver = true;
+      }
+      h1P1.innerHTML = scoreP1;
+    }
+    fWinP1();
+    fWinP2();
+  }
+});
 btnP1.addEventListener("click", () => {
   if (!gameOver) {
     scoreP1 += 1;
-    if (scoreP1 == target) {
+    if (scoreP1 === target) {
       gameOver = true;
     }
     h1P1.innerHTML = scoreP1;
   }
+  fWinP1();
+  fWinP2();
 });
 
 //----------------------------
@@ -83,14 +126,29 @@ btnP2.setAttribute(
   "ml-2 px-3 py-2 border-2 border-amber-400 hover:border-white hover:text-white rounded-lg bg-amber-400 font-semibold"
 );
 btnP2.innerHTML = "PLAYER 2<br>(+1)";
+window.addEventListener("keydown", (e) => {
+  if (e.key === ".") {
+    if (!gameOver) {
+      scoreP2 += 1;
+      if (scoreP2 === target) {
+        gameOver = true;
+      }
+      h1P2.innerHTML = scoreP2;
+    }
+    fWinP1();
+    fWinP2();
+  }
+});
 btnP2.addEventListener("click", () => {
   if (!gameOver) {
     scoreP2 += 1;
-    if (scoreP2 == target) {
+    if (scoreP2 === target) {
       gameOver = true;
     }
     h1P2.innerHTML = scoreP2;
   }
+  fWinP1();
+  fWinP2();
 });
 
 //----------------------------
@@ -109,7 +167,22 @@ function fReset() {
   h1P1.innerHTML = scoreP1;
   h1P2.innerHTML = scoreP2;
   gameOver = false;
+  winnersP1.setAttribute(
+    "class",
+    "mt-1 h-10 font-semibold text-2xl text-center"
+  );
+  winnersP2.setAttribute(
+    "class",
+    "mt-1 h-10 font-semibold text-2xl text-center"
+  );
+  winnersP1.innerHTML = "";
+  winnersP2.innerHTML = "";
 }
+window.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    fReset();
+  }
+});
 btnReset.addEventListener("click", fReset);
 btnReset.innerHTML = "RESET";
 
@@ -124,10 +197,10 @@ targetChange.addEventListener("change", () => {
 //----------------------------
 
 boxSp1.append(h1P1);
-labelP1.append(lValueP1, boxSp1);
+labelP1.append(lValueP1, boxSp1, winnersP1);
 
 boxSp2.append(h1P2);
-labelP2.append(lValueP2, boxSp2);
+labelP2.append(lValueP2, boxSp2, winnersP2);
 
 boxSp2.append(h1P2);
 score.append(labelP1, vs, labelP2);
